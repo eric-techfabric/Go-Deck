d0941f7e045e649495d621c31d3f2378afc61929
eric-techfabric <eric@techfabric.co>
2019-01-24 21:17:49 -0700

    Read + write deck to/from system files

--- deck.go
+++ deck.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
 
 // Create a new type of deck which is a slice of strings
 type deck []string
@@ -20,8 +25,7 @@ func newDeck() deck {
 	return cards
 }
 
-// Create a function that basically says: Any variable of type "deck"
-// gets access to this print function
+// Create a function that basically says: Any variable of type "deck" gets access to this print function
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
@@ -30,4 +34,22 @@ func (d deck) print() {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		os.Exit(1)
+	}
+	s := strings.Split(string(bs), ",")
+	return deck(s)
 }
--- main.go
+++ main.go
@@ -2,12 +2,6 @@ package main
 
 func main() {
 	// Instantiate new deck of cards
-	cards := newDeck()
-
-	// Deal returns two values, so we assign the first returned value
-	// to hand, and the second to remaining cards
-	hand, remainingCards := deal(cards, 5)
-
-	hand.print()
-	remainingCards.print()
+	cards := newDeckFromFile("my_cards")
+	cards.print()
 }
